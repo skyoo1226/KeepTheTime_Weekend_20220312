d1ae1db7921b964c8449113ceebd4cad13054c04
nepp.plus <nepp.plus@gmail.com>
2022-03-12 16:09:18 +0900

    BaseActivity에서 API 세팅 코드 추가  - 상속받은 모든 화면에서 apiList 변수를 통하기만 하면 API 기능 수행 가능

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/BaseActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/BaseActivity.kt
@@ -3,8 +3,11 @@ package com.neppplus.keepthetime_weekend_20220312
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.neppplus.keepthetime_weekend_20220312.api.APIList
+import com.neppplus.keepthetime_weekend_20220312.api.ServerAPI
 
 // 상속 활용 : 우리화면 >  BaseActivity()  > AppCompatActivity()
+// 모든 화면이 공통적으로 사용할 것들 : 여기에서 코딩. 상속을 통해 쉽게 사용 가능.
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -12,12 +15,20 @@ abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext: Context
 
+//    apiList : 앱에서 활용할 수 있는 API 목록.
+    lateinit var apiList: APIList
+
 //    다른 화면들의 super.onCreate가 실행될때, 부가적으로 실행해줄 코드들 추가.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mContext = this  // this 변수를 사용할 상황 : 무조건 mContext를 대신 사용.
+
+//        apiList 변수에 세팅
+        val retrofit = ServerAPI.getRetrofit()
+        apiList = retrofit.create(APIList::class.java)
+
     }
 
 //    함수 - setupEvents / setValues 모든 화면이 (각각 내용이 다르게) 구현.
