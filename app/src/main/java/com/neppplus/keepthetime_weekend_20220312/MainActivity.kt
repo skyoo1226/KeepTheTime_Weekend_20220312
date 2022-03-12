4ac2f36d284817b364d64066257c45259f4c056b
nepp.plus <nepp.plus@gmail.com>
2022-03-12 11:44:28 +0900

    데이터바인딩 - kt에서 binding 변수 생성, setContentView의 결과로 대입

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/MainActivity.kt
@@ -2,10 +2,16 @@ package com.neppplus.keepthetime_weekend_20220312
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_weekend_20220312.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    binding : 어떤 xml을 접근하는지. 자료형으로 설정.
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
