83696f0db3b6a66fb1cdc0d06e02eaa0344b02b8
nepp.plus <nepp.plus@gmail.com>
2022-03-12 15:37:01 +0900

    회원가입화면의 데이터바인딩 세팅

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/SignUpActivity.kt
@@ -2,10 +2,15 @@ package com.neppplus.keepthetime_weekend_20220312
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_weekend_20220312.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
     }
 }
