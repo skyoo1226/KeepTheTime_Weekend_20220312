dfbe4254fbc8047fd0d372f3b8d4340a8fe8f820
nepp.plus <nepp.plus@gmail.com>
2022-03-12 14:32:44 +0900

    formData에 데이터 첨부 : @FormUrlEncoded 문구 추가

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/api/APIList.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/api/APIList.kt
@@ -3,6 +3,7 @@ package com.neppplus.keepthetime_weekend_20220312.api
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 // keepthetime.xyz  서버에 있는 기능에 접속하는 방법을 명시하는 인터페이스.
@@ -11,6 +12,7 @@ interface APIList {
 
 //    로그인 기능 :  POST - /user
 
+    @FormUrlEncoded  // POST / PUT / PATCH - formData(앱코드: Field) 에 데이터 첨부시에 필요한 코드
     @POST("/user")
     fun postRequestLogin(
         @Field("email") id: String,
