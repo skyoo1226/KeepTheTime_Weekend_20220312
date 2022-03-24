483cd3dd9204c702458c1847f8274f938ce29da0
skyoo1226 <you1226@naver.com>
2022-03-24 17:41:35 +0900

    SimpleDateFormat을 활용 선택된 일자를 "M월 d일" 형태로 출력.

--- app/src/main/java/com/skyoo/keepthetime_weekend_20220312/EditAppointmentActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_weekend_20220312/EditAppointmentActivity.kt
@@ -1,13 +1,20 @@
 package com.skyoo.keepthetime_weekend_20220312
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.skyoo.keepthetime_weekend_20220312.databinding.ActivityEditAppointmentBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditAppointmentActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditAppointmentBinding
+
+    val mSelectedDatetimeCal = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,30 @@ class EditAppointmentActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.txtDate.setOnClickListener {
+            val dsl = object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//                    Toast.makeText(mContext, "${year}년 ${month}월 ${dayOfMonth}일", Toast.LENGTH_SHORT).show()
+                    mSelectedDatetimeCal.set(year, month, dayOfMonth)
+
+//      약속 일자 텍스트뷰의 문구를 "00월 00일" 형태로 가공해서 출력.
+                    val sdf = SimpleDateFormat("M월 d일")
+
+                    binding.txtDate.text = sdf.format(mSelectedDatetimeCal.time)
+                }
+            }
+
+            val dpd = DatePickerDialog(
+                mContext,
+                dsl,
+                mSelectedDatetimeCal.get(Calendar.YEAR),
+                mSelectedDatetimeCal.get(Calendar.MONTH),
+                mSelectedDatetimeCal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+
+        }
 
     }
 
