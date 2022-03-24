e11c61e2ced8151c3fcf1c37ecaabc13625b1220
skyoo1226 <you1226@naver.com>
2022-03-24 20:31:00 +0900

    마커의 크기, 색상 등 조정 가능. 마커 객체 하나를 멘버변수로 적용 - 초기값을 null, null일때만 처음 적용, 그 뒤에 위치만 옯김.

--- app/src/main/java/com/skyoo/keepthetime_weekend_20220312/EditAppointmentActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_weekend_20220312/EditAppointmentActivity.kt
@@ -2,6 +2,7 @@ package com.skyoo.keepthetime_weekend_20220312
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
@@ -10,6 +11,8 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.MarkerIcons
 import com.skyoo.keepthetime_weekend_20220312.databinding.ActivityEditAppointmentBinding
 import java.text.SimpleDateFormat
 import java.util.*
@@ -19,6 +22,9 @@ class EditAppointmentActivity : BaseActivity() {
     lateinit var binding: ActivityEditAppointmentBinding
 
     val mSelectedDatetimeCal = Calendar.getInstance()
+
+    //  지도에 띄워줄 목적지 표시해줄 마커 변수.
+    var myMarker : Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,6 +86,34 @@ class EditAppointmentActivity : BaseActivity() {
             val naverMap = it
             val cameraUpdate = CameraUpdate.scrollTo((LatLng(37.589730475241765, 127.05857781302444)))
             naverMap.moveCamera(cameraUpdate)
+
+            val marker = Marker()
+            marker.position = LatLng(37.589730475241765, 127.05857781302444)
+            marker.map = naverMap
+
+            marker.icon = MarkerIcons.BLACK
+            marker.iconTintColor = Color.BLUE
+            marker.width = 50
+            marker.height = 80
+
+            naverMap.setOnMapClickListener { pointF, latLng ->
+//      클릭된 좌표(위치)를 위도와 경도로 표시 함.
+//                Toast.makeText(
+//                    mContext,
+//                    "위도 : ${latLng.latitude}, 경도 : ${latLng.longitude}",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+
+                if (myMarker == null) {
+                    myMarker = Marker()
+                }
+                myMarker!!.position = latLng
+                myMarker!!.map = naverMap
+
+//              val myMarker = Marker()   맨 위에 var변수로  myMarker를 사용 했기 때문에 지움.
+//              myMarker.position = latLng
+//              myMarker.map = naverMap
+            }
         }
 
     }
