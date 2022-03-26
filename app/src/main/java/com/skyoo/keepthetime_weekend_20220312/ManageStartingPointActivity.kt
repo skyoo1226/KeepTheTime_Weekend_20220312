e9b2acfb6783c5a0262e5802921eb9e0735d37b5
skyoo1226 <you1226@naver.com>
2022-03-26 18:01:12 +0900

    데이터 클래스 구조만 설정 -> JSONObject/JSONArray 등에 대한 파싱 자동 완료

--- app/src/main/java/com/skyoo/keepthetime_weekend_20220312/ManageStartingPointActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_weekend_20220312/ManageStartingPointActivity.kt
@@ -2,10 +2,57 @@ package com.skyoo.keepthetime_weekend_20220312
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.skyoo.keepthetime_weekend_20220312.databinding.ActivityManageStartingPointBinding
+import com.skyoo.keepthetime_weekend_20220312.datas.BasicResponse
+import com.skyoo.keepthetime_weekend_20220312.datas.StartingPointData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class ManageStartingPointActivity : AppCompatActivity() {
+class ManageStartingPointActivity : BaseActivity() {
+
+    lateinit var binding: ActivityManageStartingPointBinding
+
+    val mStartingPointList = ArrayList<StartingPointData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_manage_starting_point)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_starting_point)
+        setupEvents()
+        setValues()
     }
+    override fun setupEvents() {
+    }
+    override fun setValues() {
+
+        txtTitle.text = "출발지 목록 관리"
+
+        getMyStartingPointFromServer()
+
+    }
+
+    fun getMyStartingPointFromServer() {
+
+        apiList.getRequestMyStartingPoint().enqueue( object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                val br = response.body()!!  // JSONObject / JSONArray 등의 중간 형태 skip. 바로 일반 클래스로 담아줌.
+
+//                ArrayList의 for문 예시
+                for ( myPlace in  br.data.places ) {
+                    Log.d("내출발장소", myPlace.name)
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
+    }
+
 }
